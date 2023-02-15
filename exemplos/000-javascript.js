5aa44e036e81528e1514d13d9778517b42f2a1a0
carlapiresc3briano <125502947+carlapiresc3briano@users.noreply.github.com>
2023-02-15 17:29:58 -0300

    variáveis

---
+++ exemplos/000-javascript.js
@@ -0,0 +1,29 @@
+// o comando abaixo apresenta uma mensagem na tela 
+console.log('olá estou programando em javascript');
+
+/*
+    comentário que ocupa mais 
+    de uma linha 
+*/
+// criação de variável do tipo texto 
+let nome = 'Carla Pires';
+// criação de variável do tipo numérico (inteiro)
+let idade = 17;
+// criação de variável do tipo númerico (decimal)
+let altura = 1.75;
+// criação de variável do tipo verdadeiro/falso
+let trabalha = true; // true = verdadeiro | false = falso
+// exemplo de variávelde nome composto
+let corFavorita = 'Laranja';
+// exemplo de variável do tipo lista de valores
+let cores_favoritas = ['laranja', 'Roxo', 'Azul'];
+// exemplo de variável que armazena um objeto
+let endereço ={
+    'logradouro'        : 'Rua',
+    'nome_logradouro'   : 'Brasil',
+    'número'            : '50 A',
+    'bairro'            : 'Centro',
+    'cidade'            : 'Tupã'
+}; 
+let covid;
+let dengue = null;
