3c02628b9135a33d0076b7efd585e0ed12a32ee7
carlapiresc3briano <125502947+carlapiresc3briano@users.noreply.github.com>
2023-03-01 17:25:02 -0300

    aula 01-03
    Saída variáveis

--- exemplos/000-javascript.js
+++ exemplos/000-javascript.js
@@ -18,7 +18,7 @@ let corFavorita = 'Laranja';
 // exemplo de variável do tipo lista de valores
 let cores_favoritas = ['laranja', 'Roxo', 'Azul'];
 // exemplo de variável que armazena um objeto
-let endereço ={
+let endereco ={
     'logradouro'        : 'Rua',
     'nome_logradouro'   : 'Brasil',
     'número'            : '50 A',
@@ -27,3 +27,14 @@ let endereço ={
 }; 
 let covid;
 let dengue = null;
+
+console.log('Nome:  '  + nome);
+console.log('Idade: '  + idade);
+console.log('Altura: ' + altura );
+console.log('Trabalha: ' + trabalha );
+console.log('CorFavorita: ' + corFavorita);
+console.log('Cores_Favoritas: ' + cores_favoritas[0]  + ', ' + cores_favoritas[1] + ', ' + cores_favoritas[2]);
+console.log('Endereço: ' + endereco.logradouro + ' ' + endereco.nome_logradouro + ', '+ endereco.número + ' - ' + endereco.bairro + ' - ' + endereco.cidade);
+console.log('Covid: ' + covid);
+console.log('Dengue:' + dengue); 
+
