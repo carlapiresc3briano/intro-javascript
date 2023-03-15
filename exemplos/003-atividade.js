5ee6604f8f92c9cb7db0267b555593ec42841510
carlapiresc3briano <125502947+carlapiresc3briano@users.noreply.github.com>
2023-03-15 16:04:01 -0300

    Create 003-atividade.js

---
+++ exemplos/003-atividade.js
@@ -0,0 +1,6 @@
+// crie um programa que receba seu nome e sua idade 
+// e apresente na saída qual será sua idade daqui
+// a 25 anos 
+// Saída - "Carla daqui a 25 anos sua idade será 50"
+
+let entrada 
