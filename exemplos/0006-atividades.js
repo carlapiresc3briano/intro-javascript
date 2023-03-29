122a3adc4e8fae237c409e86300b442c5043ad04
carlapiresc3briano <125502947+carlapiresc3briano@users.noreply.github.com>
2023-03-29 17:39:34 -0300

    aula 7

---
+++ exemplos/0006-atividades.js
@@ -0,0 +1,25 @@
+ // Estrutura condicionais 
+
+ const entrada = require('prompt-sync')();
+
+ let idadeTxt= entrada('Digite sua idade: ');
+ let idade = parseInt(idadeTxt);
+
+ if (idade > 18) {
+     console.log('Voçe tem mais de 18 anos! ');
+ }
+
+ if (idade < 18)  {
+     console.log('Voçe tem menos de 18 anos!');
+
+ }
+ if (idade < 18)  {
+    console.log('Voçe tem menos de 18 anos!');
+ }
+
+  if (idade < 18)  {
+     console.log('Voçe tem menos de 18 anos!'); 
+    }
+    
+  
+  
