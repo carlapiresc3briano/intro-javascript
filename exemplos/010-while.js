857674294b576274f113ff3a719b0df483e6c503
carlapiresc3briano <125502947+carlapiresc3briano@users.noreply.github.com>
2023-04-26 17:27:42 -0300

    aula 26/04/23

---
+++ exemplos/010-while.js
@@ -0,0 +1,9 @@
+
+// Estrutura de repetição com teste no ínicio
+
+let contador = 1;
+
+while (contador < 11) {
+    console.log(contador);
+    contador = contador + 1;
+}
---
+++ exemplos/014-for-time.js
@@ -0,0 +1,9 @@
+
+for (let i = 1; i <= 10; i++) {
+    setTimeout(function() {
+        console.log(i);
+    
+    }
+    )
+  
+}
