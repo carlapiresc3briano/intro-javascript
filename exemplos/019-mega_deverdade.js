f1e516e8defd6fabad61a68f009fef17b2d38b12
carlapiresc3briano <125502947+carlapiresc3briano@users.noreply.github.com>
2023-05-17 17:27:41 -0300

    aula 17-05

--- exemplos/019-mega_deverdade.js
+++ exemplos/019-mega_deverdade.js
@@ -10,8 +10,13 @@ let resMegaSena = [5, 15, 25, 35, 45, 55];
 let meuJogo = [1, 2, 3, 4, 5, 6];
 let acertos = 0;
 
-for (let 1 = 0; 1 < 6; i++) {
-    let numero = entrada('Digite o ' + n + 'º nº do jogo: ');
+for (let i = 0; i <6; 1++) {
+    const numero = Math.floor(Math.random() * 60) + 1;
+    resMegaSena.push(numero);
+}
+
+for (let i = 0; 1 < 6; i++) {
+    let numero = entrada('Digite o ' + i + 'º nº do jogo: ');
     meuJogo.push(numero);
 }
 
@@ -21,6 +26,21 @@ for (let nMega = 0; nMega < resMegaSena.length; nMega++) {
             acertos++;
         }
     }
+}
+
+let mensagem = 'Resultado do sorteio: ';
+
+for (let n = 0; n < resMegaSena.length; n++) {
+    mensagem = mensagem + resMegaSena[n] + 
+    (resMegaSena.length == n + 1 ? '' : ' - ');
+
+}
+
+mensagem = 'Seu jogo: ';
+
+for (let n = 0; < meuJogo.length; n++) {
+    mensagem = mensagem + meujogo[n] +
+    (meuJogo.length == n + 1 ? '' : ' - ');
 }
 
 console.log('Você acertou ' + acertos + 'número' + (acertos > 1 ? 's' : '' )) ;
